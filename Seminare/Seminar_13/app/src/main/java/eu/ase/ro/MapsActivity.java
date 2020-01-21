93008239c26e29df9ecd6a2af4af8a995e25fa7d
deodre <45459027+deodre@users.noreply.github.com>
2020-01-22 00:15:36 +0200

    added lab 13

---
+++ Seminare/Seminar_13/app/src/main/java/eu/ase/ro/MapsActivity.java
@@ -0,0 +1,73 @@
+package eu.ase.ro;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LatLng s2001;
+    List<LatLng> punctePoligon = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+                // mMap.addPolyline(new PolylineOptions().add(latLng).add(s2001));
+
+                punctePoligon.add(latLng);
+                PolygonOptions polygonOptions = new PolygonOptions();
+                for(LatLng punctPoligon: punctePoligon) {
+                    polygonOptions.add(punctPoligon);
+                    mMap.addMarker(new MarkerOptions().position(punctPoligon));
+                }
+
+                mMap.addPolygon(polygonOptions);
+            }
+        });
+
+        s2001 = new LatLng(44.447654, 26.099093);
+        mMap.addMarker(new MarkerOptions().position(s2001).title("Marker in sala 2001"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(s2001, 18));
+    }
+}
